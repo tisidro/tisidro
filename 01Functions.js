3afa0c08b7728b3698e28b2fa242176d3100213f
TerriIsidro <49820744+tisidro@users.noreply.github.com>
2021-04-22 10:33:01 -0400

    Add files via upload

---
+++ 01Functions.js
@@ -0,0 +1,19 @@
+//node 03Intermediate/01Functions.js
+
+//function sayHello(name){
+  //  console.log(`Hello there, ${name}. How are you?`);
+//}
+
+//sayHello("Fran");
+//sayHello("Sharon");
+//parenthesis means calling the function
+
+function howdy() {
+    return "What's going on?"
+}
+//storing a function inside a variable
+var greetings = howdy();
+
+console.log(greetings);
+//or can use
+console.log(howdy());
